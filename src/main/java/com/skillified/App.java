88d2eb6ea07b7a03e334f7c4eea04f430fc2e001
Ubuntu <ubuntu@ip-172-31-22-65.ec2.internal>
2026-06-15 08:23:25 +0000

    Intial Maven Build Automation Project

---
+++ src/main/java/com/skillified/App.java
@@ -0,0 +1,8 @@
+package com.skillified;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Build Automation Using Maven");
+        System.out.println("Skillified Cloud & DevOps Internship Project");
+    }
+}
